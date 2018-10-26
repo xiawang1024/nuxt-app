73c64c5c89e130f9ddf511bd761e1baf39e29973
wangxia/work <xiawang1024@icloud.com>
2018-10-26 16:42:23 +0800

    feat:first

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,19 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
-        nuxt-app
+        Hello!
       </h1>
       <h2 class="subtitle">
-        My exquisite Nuxt.js project
+        WangXia
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
 
 <style>
-
 .container {
   min-height: 100vh;
   display: flex;
